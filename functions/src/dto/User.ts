0629819b959aa35a38c9049f8ee0ff294bf4715b
parkjunsik <akfldkskgorn@gmail.com>
2024-05-20 13:57:10 +0900

    CHC - 14 | feat : question에 chat gpt 연동 완료

--- functions/src/dto/User.ts
+++ functions/src/dto/User.ts
@@ -3,17 +3,19 @@ export class User {
   nickname: string;
   currentRid: string | null;
   experience: number;
-
+  isOnline: boolean;
 
   constructor(
     uid: string,
     nickname: string,
     currentRid: string | null,
-    experience: number
+    experience: number,
+    isOnline: boolean
   ) {
     this.uid = uid;
     this.nickname = nickname;
     this.currentRid = currentRid;
     this.experience = experience;
+    this.isOnline = isOnline;
   }
 }
